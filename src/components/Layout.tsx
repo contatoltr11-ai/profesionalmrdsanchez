6672badfdc9a8757beafcbe996ca0d24b626c8fc
adeliaterco <adeliarosaterco@gmail.com>
2026-09-04 15:22:26 -0300

    jjdjd

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -64,12 +64,7 @@ export default function Layout({ children }: LayoutProps) {
     // ========================================
     // ✅ UTMIFY PIXEL
     // ========================================
-    window.pixelId = "699fb21f66b4e999b2645ba9";
-    const utmifyPixelScript = document.createElement("script");
-    utmifyPixelScript.async = true;
-    utmifyPixelScript.defer = true;
-    utmifyPixelScript.src = "https://cdn.utmify.com.br/scripts/pixel/pixel.js";
-    document.head.appendChild(utmifyPixelScript);
+
 
     // ========================================
     // ✅ UTMIFY UTM TRACKER
