24cd2168d3ce7bb58a2ad311b7115da567631333
adeliaterco <adeliarosaterco@gmail.com>
2026-09-04 17:38:17 -0300

    hhdh

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -64,7 +64,7 @@ export default function Layout({ children }: LayoutProps) {
     // ========================================
     // ✅ UTMIFY PIXEL
     // ========================================
-    window.pixelId = "699fb21f66b4e999b2645ba9";
+    window.pixelId = "6a9ae6756c5aacc346669146";
     const utmifyPixelScript = document.createElement("script");
     utmifyPixelScript.async = true;
     utmifyPixelScript.defer = true;
